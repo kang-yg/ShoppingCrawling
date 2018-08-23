24fc183f6636d77a7f4107e3d47b9839ed8beaa9
younggyu <kinny>
2018-08-24 00:16:21 +0900

    코스트코 음료, 과일, 스낵 + 이마트&롯데몰 작업중...

---
+++ ShoppingCrawling/CostcoCrawling/src/CostcoSnack.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class CostcoSnack {
+	public static void main(String[] args) {
+		try {
+			Document doc = Jsoup.connect("https://www.costco.co.kr/Food/Snack/c/cos_10.5").get();
+			Elements hotdeal = doc.select(".product-listing.product-grid");
+			Elements deal = hotdeal.select("li");
+			
+			for(Element e : deal) {
+				System.out.println(e.text());
+			}
+			System.out.println("------------------------------1∆‰¿Ã¡ˆ------------------------------------------");
+		}catch (IOException e) {e.printStackTrace();
+		}
+	}
+}
---
+++ ShoppingCrawling/LootteMallCrawling/src/LotteMallDrink.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class LotteMallDrink {
+	public static void main(String[] args) {
+		try {
+			Document doc = Jsoup.connect("http://www.lottemart.com/category/categoryList.do?CategoryID=C0010017").get();
+			Elements hotdeal = doc.select("");
+			Elements deal = hotdeal.select("");
+			
+			for(Element e : deal) {
+				System.out.println(e.text());
+			}
+			System.out.println("------------------------------1∆‰¿Ã¡ˆ------------------------------------------");
+		}catch (IOException e) {e.printStackTrace();
+		}
+	}
+}
